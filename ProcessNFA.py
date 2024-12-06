27aa7a8b56dacafe84a75674bd55b03be8ecc3eb
Italo Vicente <114747617+ItaloVicente@users.noreply.github.com>
2024-12-06 15:33:46 -0300

    Add files via upload

--- ProcessNFA.py
+++ ProcessNFA.py
@@ -1,4 +1,4 @@
-
+from itertools import combinations
 
 def return_txt(nome_arquivo,q,alfabeto,funcs,q_inicial,f,cabecalho):
     with open(nome_arquivo + ".txt", "w") as file:
@@ -39,8 +39,56 @@ def return_txt(nome_arquivo,q,alfabeto,funcs,q_inicial,f,cabecalho):
         file.write(estados_finais)
         file.close()
 
-def afnd_to_afd(q,alfabeto,funcs,q_inicial,f):
-    pass
+def epsilon_closure_q0(estado, funcs):
+    # Inicia o fecho com o próprio estado
+    fecho = [estado]
+    # Pilha para explorar novos estados
+    stack = [estado]
+
+    while stack:
+        current = stack.pop()
+        # Verificar se há transições "E" a partir do estado atual
+        for transicoes in funcs["funcoes"][current]:
+            if "E" in transicoes[0]:
+                prox_estado = transicoes[1]
+                if prox_estado not in fecho and prox_estado in funcs["funcoes"]:
+                    fecho.append(prox_estado)
+                    stack.append(prox_estado)
+                elif prox_estado not in fecho and prox_estado not in funcs["funcoes"]:
+                    fecho.append(prox_estado)
+    new_q0 = "{"
+    for i, estado in enumerate(fecho):
+        if i != len(fecho)-1:
+            new_q0 = new_q0 + estado + ", "
+        elif i == len(fecho)-1:
+            new_q0 = new_q0 + estado + "}"
+    return new_q0
+def afnd_to_afd(q, alfabeto, funcs, q_inicial, f):
+    new_q = {"Q": []}
+    new_funcs = {"funcoes": {}}
+    new_f = {"F": []}
+    new_q0 = {"q0": []}
+
+    # Gerar todas as combinações possíveis de estados
+    estados = q["Q"]
+    for tamanho in range(1, len(estados) + 1):  # Subconjuntos de tamanhos 1 até len(estados)
+        for combinacao in combinations(estados, tamanho):
+            conjunto_str = "{" + ", ".join(combinacao) + "}"
+            new_q["Q"].append(conjunto_str)
+    #estado vazio
+    new_q["Q"].append("∅")
+    # Verificar quais estados são finais
+    for estado in new_q["Q"]:
+        partes_estado = estado.strip("{}").split(", ")
+        for elemento in partes_estado:
+            if (elemento in f["F"]):
+                new_f["F"].append(estado)
+    #q0 = E_fecho(q0)
+    new_q0["q0"] = epsilon_closure_q0("q0", funcs)
+    # Novas funcoes
+
+
+    return new_q, alfabeto, new_funcs, new_q0, new_f
 
 def check_dicts_txt(dicionario, chave, linha_tratada):
     for i in range(1, len(linha_tratada)):
@@ -105,4 +153,5 @@ print(funcs)
 print(q_inicial)
 print(f)
 
-return_txt("AFN",q,alfabeto,funcs,q_inicial,f, "# AFN Original \n")
+#return_txt("AFN",q,alfabeto,funcs,q_inicial,f, "# AFN Original \n")
+print(afnd_to_afd(q,alfabeto,funcs,q_inicial,f))
